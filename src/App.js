3635486599b8feea4533c9173ae81761afdbd93c
John Manack <jmnck28@gmail.com>
2021-02-24 13:03:06 -0500

    adds first props and fragment tags in JSX

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Hello(props) {
+  return (
+    <>
+      <h1 className="title">Hello {props.name}!</h1>
+      <h2>SUBTITLE</h2>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello name="John" />
       </header>
     </div>
   );
